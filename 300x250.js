cacb73c3791a198519fbdba2ff7e51738dd699cd
botrobot320 <147258215+botrobot320@users.noreply.github.com>
2023-11-04 22:38:21 -0700

    Add files via upload

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '06e72bf327eb1284eb8ed0507189d7f0',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/06e72bf327eb1284eb8ed0507189d7f0/invoke.js"></scr' + 'ipt>');
---
+++ 90x728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c2d4b83d50625e876390f901f719bb35',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/c2d4b83d50625e876390f901f719bb35/invoke.js"></scr' + 'ipt>');
